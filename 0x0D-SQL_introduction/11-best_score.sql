a77047ab8ca9da216eb9dce6580ddd76e6451a47
seyfu tsegaye <99539647+seyfutsegaye@users.noreply.github.com>
2023-02-14 15:51:30 +0300

    Task11

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- from the database hbtn_0c_0
+-- lists all records with a score >= 10
+-- display both the score and the name
+-- ordered by score
+-- passed as an argument
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
