5d60ef77b695af213dedbf0cd153f7303de12b3e
seyfu tsegaye <99539647+seyfutsegaye@users.noreply.github.com>
2023-03-14 10:25:29 +0300

    Task1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // scipt that print three lines of code 
-const  myVar = 'C is fun \n Python is cool \n JavaScript is amazing ';
+const  myVar = 'C is fun\n Python is cool\nJavaScript is amazing ';
 console.log(myVar);
