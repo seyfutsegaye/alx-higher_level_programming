00a5bb67ac3ae4188a08bc35b4c060d6b6d8242b
seyfu tsegaye <99539647+seyfutsegaye@users.noreply.github.com>
2023-04-25 15:33:03 +0300

    Task4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) throw new Error(error);
+  let count = 0;
+  JSON.parse(body).results.forEach(film => {
+    film.characters.forEach(character => {
+      const res = character.includes('/18/');
+      count += (res ? 1 : 0);
+    });
+  });
+  console.log(count);
+});
